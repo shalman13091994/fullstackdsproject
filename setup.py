901ee10846d8bf6dd3e09b010b44e7d7956e8752
shalman <shalman13091994@gmail.com>
2025-03-26 17:42:22 +0530

    completed till model evaluation

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
 from setuptools import find_packages,setup
 from typing import List
 
-"""HYPEN_E_DOT='-e .'S
+HYPEN_E_DOT='-e .'
 
-def get_requirements(file_path:str)->List[str]:
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+# def get_requirements(file_path:str)->List[str]:
+#     requirements=[]
+#     with open(file_path) as file_obj:
+#         requirements=file_obj.readlines()
+#         requirements=[req.replace("\n","") for req in requirements]
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+#         if HYPEN_E_DOT in requirements:
+#             requirements.remove(HYPEN_E_DOT)
 
-    return requirements"""
+#     return requirements
 
 setup(
     name='DiamondPricePrediction',
     version='0.0.1', 
     author='shalman.samiulla',
     author_email='shalman13091994@gmail.com',
-    install_requires=["scikit-learn","pandas","numpy"],
+    install_requires=["scikit-learn","pandas","numpy"], #comment the get_requirements()
+    # install_requires = get_requirements(requirements.txt),
     packages=find_packages()
 )
 
